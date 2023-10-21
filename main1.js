71752c9f26fd9176ed9dbf96e93d866665406ad4
EzerKadang <ezerkadang@gmail.com>
2023-10-21 23:45:02 +0800

    first commit

---
+++ main1.js
@@ -0,0 +1,86 @@
+// let nama = "Eliezer Butu Kadang Sirama";
+// let umur = 25;
+// let tinggiBadan = 175.5;
+// let beratBadan = 60;
+// let pacar = 1;
+
+// Condition statemane if
+// if (pacar == null) {
+//   pacar = "saya tidak punya pacar";
+// } else {
+//   pacar = "saya punya 1 pacar";
+// }
+
+// Condition statemane switch
+// switch (pacar) {
+//   case 1:
+//     pacar = "Saya punya pacar 1";
+//     break;
+//   case 2:
+//     pacar = "saya punya pacar 2";
+//     break;
+//   default:
+//     pacar = "saya tidak punya pacar";
+//     break;
+// }
+
+// const saldoAwal = 100000;
+// const saldoTambahan = 50000;
+// const hutang = 80000;
+
+// let totalSaldo = saldoAwal + saldoTambahan - hutang;
+
+// alert(
+//   `nama saya ${nama}, umur saya ${umur}, tinggi badan saya ${tinggiBadan} cm, berat badan ${beratBadan}, dan pacar ${pacar}`
+// );
+
+// alert(
+//   `saldo saya adalah Rp.${saldoAwal} saldo tambahan saya Rp.${saldoTambahan} sisa saldo saya Rp.${totalSaldo}`
+// );
+
+// Array
+// let namaGuru = ["Jodi", "Bunga", "Riki"];
+// namaGuru.push("radja", "radtu"); //menambahkan array
+// namaGuru.shift(); //menghapus array pertama
+// namaGuru.pop(); //menghapus array terakhir
+// alert(namaGuru);
+
+// for loop
+// let namaGuru = ["Jodi", "Bunga", "Riki"];
+
+// for (let i = 0; i < namaGuru.length; i++) {
+//   console.log(namaGuru[i]);
+// }
+
+// let i = 9;
+// while
+// while (i < 10) {
+//   i++;
+//   console.log("eser");
+// }
+
+// do while
+// do {
+//   console.log("eser");
+//   i++;
+// } while (i < 10);
+
+// tugas
+// 1.promt untuk mengetahui saldo akhir dari yang diinputkan oleh user
+// let saldo = prompt("saldo anda adalah");
+// console.log(`saldo anda adalah ${saldo}`);
+
+// 2. menentukan hari dan tanggal yang ada di komputer dan pc
+
+// let day = new Date();
+// const today = day.getDay();
+// const dayName = [
+//   "senin",
+//   "selasa",
+//   "rabu",
+//   "kamis",
+//   "jum'at",
+//   "sabtu",
+//   "minggu",
+// ];
+// console.log(`hari ini adalah ${dayName[today]}`);
---
+++ main2.js
@@ -0,0 +1,5 @@
+console.log(document.title);
+
+document.title = "eserK";
+
+console.log(document.title);
